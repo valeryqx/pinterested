991d72235b43970eaf62cca72e1fc0b21be5dc5d
Valery <valeryqx@gmail.com>
2016-04-07 13:46:58 +0300

    production host fixed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,5 +77,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'pinterested135.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'https://pinterested135.herokuapp.com/' }
 end
